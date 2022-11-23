3c51260ddf069611075ae859c6dc479dcab54eda
Justin Litchfield <litch@me.com>
2022-11-23 10:59:18 -0600

    Operator works in a container

--- provide-authentitation-to-operator.sh
+++ provide-authentitation-to-operator.sh
@@ -5,6 +5,9 @@ for node in "${lnd_nodes[@]}"
 do
     cp volumes/$node/tls.cert operator/server/auth/$node.cert
     cp volumes/$node/data/chain/bitcoin/regtest/admin.macaroon operator/server/auth/$node.macaroon
+    cp volumes/$node/tls.cert volumes/operator/$node.cert
+    cp volumes/$node/data/chain/bitcoin/regtest/admin.macaroon volumes/operator/$node.macaroon
+    
     cp volumes/$node/tls.cert volumes/thunderhub/auth/$node.cert
     cp volumes/$node/data/chain/bitcoin/regtest/admin.macaroon volumes/thunderhub/auth/$node.macaroon
 
